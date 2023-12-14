c26b853814e2d7a4f5e8637546065eb4157050e3
216-coding-ninja <tumelo.mahlangu@216.co.za>
2023-12-14 13:06:57 +0200

    updated assets

---
+++ src/pages/HeroDetails/HeroEvent.jsx
@@ -0,0 +1,58 @@
+import { Colors } from '@/common/constants';
+import{
+    styled,
+    Box,
+    Typography
+} from '@mui/material';
+
+
+const EventBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2)
+}))
+
+const LabelBox = styled(Box)(({ theme }) => ({
+    padding: theme.spacing(0.2, 1),
+    backgroundColor: Colors.Purple,
+    width: 'max-content'
+}))
+
+const LabelText = styled(Typography)(({ theme }) => ({
+    fontSize: '11px',
+    fontWeight: '600',
+    color: Colors.White
+}))
+
+const Title = styled(Typography)({
+    fontSize: '16px',
+    color: Colors.White,
+    fontWeight: '600',
+    width: '200px'
+})
+
+const SettingTitle = styled(Typography)({
+    color: Colors.Grey,
+    fontSize: '14px',
+})
+
+function HeroEvent({label, title, setting}){
+    return(
+        <EventBox>
+            <LabelBox>
+                <LabelText>
+                    {label}
+                </LabelText>
+            </LabelBox>
+            <Title>
+                {title}
+            </Title>
+            <SettingTitle>
+                {setting}
+            </SettingTitle>
+        </EventBox>
+    )
+
+}
+
+export default HeroEvent;
