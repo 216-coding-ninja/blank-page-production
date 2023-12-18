f33852eb25e753a817d23cda6b5fc0bdf8bc5db5
216-coding-ninja <tumelo.mahlangu@216.co.za>
2023-12-18 09:50:11 +0200

    updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import NavigationBar from "@/common/components/NavigationBar"
-import Hero from "./pages/Hero"
+import Hero from "./pages/Hero/Hero"
 
 import { createTheme, CssBaseline, ThemeProvider, styled } from "@mui/material";
 import { typography } from "./common/theme/typography";
